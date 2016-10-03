07656a0c34a27ee547864de94b5326ed71eb9277
Hassan Mahmud <Hassan Mahmud>
2016-10-03 10:37:52 +0200

    Init

---
+++ Contracts_Compulsory_Assignment/Contracts_Compulsory_Assignment/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts_Compulsory_Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number: ('q' to stop the application)");
+
+                var enteredKey = Console.ReadLine();
+
+                if (enteredKey == "q")
+                {
+                    break;
+                }
+
+                var num1 = Convert.ToInt32(enteredKey);
+
+                Console.WriteLine(IsPrime(num1) ? "It is prime" : "It is not prime");
+            }
+        }
+
+        //public static bool IsPrime()
+        //{
+        //    var number = 5;
+            
+            
+        //}
+
+        public static bool IsPrime(int number)
+        {
+            Contract.Requires(number > 0);
+            Contract.Ensures(Contract.Result<bool>() == Contract.ForAll(2, number, c => number % c != 0));
+
+            // var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            if (number == 1)
+            {
+                return false;
+            }
+            if (number == 2)
+            {
+                return true;
+            }
+            
+            for (var i = 2; i < number; ++i)
+            {
+                Contract.Assert(Contract.ForAll(2, i, c => number % c != 0));
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
